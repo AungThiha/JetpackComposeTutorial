ab7077ecdb6e49617499704904895000d88a28d8
AungThiha <aung@honestbank.com>
2022-01-02 23:15:29 +0700

    sample columns, rows and modifiers

--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -3,30 +3,58 @@ package com.jetpack.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpack.compose.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Aung")
+            val message = Message(author = "Aung", body = "Hi")
+            MessageCard(message = message)
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(message: Message) {
+    // Add padding around the Row
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                // Set image size to 40dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+        Column {
+            Text(text = message.author, color = Color.White)
+            // Add a verticle space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body, color = Color.White)
         }
     }
 }
 
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name", color = Color.White)
-}
-
-@Composable
 @Preview
 fun PreviewMessageCard() {
-    MessageCard(name = "Android")
+    val message = Message(author = "Aung", body = "Hi")
+    MessageCard(message = message)
 }
